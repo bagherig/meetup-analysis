90500412e2ba396be4dd8b24a927aa732c231ba1
Reuben Pereira <reuben.pereira126@gmail.com>
2018-12-28 02:22:12 -0500

    init

--- scripts/create_bq_tables.bash
+++ scripts/create_bq_tables.bash
@@ -7,10 +7,10 @@ bq mkdef --noautodetect --source_format=NEWLINE_DELIMITED_JSON "gs://meetup_data
 bq mkdef --noautodetect --source_format=NEWLINE_DELIMITED_JSON "gs://meetup_data/photos/*.json" ../bq_table_schema/photos.json > photos_tdf.json
 bq mkdef --noautodetect --source_format=NEWLINE_DELIMITED_JSON "gs://meetup_data/open_venues/*.json" ../bq_table_schema/open_venues.json > open_venues_tdf.json
 bq mkdef --noautodetect --source_format=NEWLINE_DELIMITED_JSON "gs://meetup_data/open_events/*.json" ../bq_table_schema/open_events.json > open_events_tdf.json
-bq mkdef --noautodetect --source_format=NEWLINE_DELIMITED_JSON "gs://meetup_data/open_events/*.json" ../bq_table_schema/open_events.json > open_events_tdf.json
+bq mkdef --noautodetect --source_format=NEWLINE_DELIMITED_JSON "gs://meetup_data/open_events/*.json" ../bq_table_schema/event_comments.json > event_comments_tdf.json
 
 jq '.+{ignoreUnknownValues:true}' rsvp_tdf.json > rsvp_tdf.json.tmp && cp rsvp_tdf.json.tmp rsvp_tdf.json
-jq '.+{ignoreUnknownValues:true}' photos.json > photos.json.tmp && cp photos.json.tmp photos.json
+jq '.+{ignoreUnknownValues:true}' photos_tdf.json > photos.json.tmp && cp photos.json.tmp photos_tdf.json
 jq '.+{ignoreUnknownValues:true}' open_venues_tdf.json > open_venues_tdf.json.tmp && cp open_venues_tdf.json.tmp open_venues_tdf.json
 jq '.+{ignoreUnknownValues:true}' open_events_tdf.json > open_events_tdf.json.tmp && cp open_events_tdf.json.tmp open_events_tdf.json
 jq '.+{ignoreUnknownValues:true}' event_comments_tdf.json > event_comments_tdf.json.tmp && cp event_comments_tdf.json.tmp event_comments_tdf.json
