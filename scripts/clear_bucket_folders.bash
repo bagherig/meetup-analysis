a499bddeb2dccc37b2c26a0c756a3263fe331bb4
Reuben Pereira <reuben.pereira126@gmail.com>
2018-12-28 17:41:22 -0500

    init

--- scripts/clear_bucket_folders.bash
+++ scripts/clear_bucket_folders.bash
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-gsutil rm -r gs://meetup_data/rsvp
-gsutil rm -r gs://meetup_data/photos
-gsutil rm -r gs://meetup_data/open_venues
-gsutil rm -r gs://meetup_data/open_events
-gsutil rm -r gs://meetup_data/event_comments
+gsutil -m rm -r gs://meetup_data/rsvp
+gsutil -m rm -r gs://meetup_data/photos
+gsutil -m rm -r gs://meetup_data/open_venues
+gsutil -m rm -r gs://meetup_data/open_events
+gsutil -m rm -r gs://meetup_data/event_comments
