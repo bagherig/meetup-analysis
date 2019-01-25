8be864b161f5df3ab273c6cc15d2364380a720ee
Moeen Bagheri <moeen_bags@hotmail.com>
2019-01-25 00:31:16 -0500

    Removed unnecessary imports.

--- scripts/custom_typing.py
+++ scripts/custom_typing.py
@@ -1,8 +1,7 @@
 # !/usr/bin/python
 # -*- coding: utf-8 -*-
 """Typing information for BCM."""
-from typing import List, Dict, Union, NewType
+from typing import NewType
 from google.cloud import logging
 
 Logger = NewType('Logger', logging.logger)
-
